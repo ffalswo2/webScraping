cea9e6eb8370c3b8f4c3247d3bc900f4d050bdce
ffalswo2 <60292150+ffalswo2@users.noreply.github.com>
2020-05-23 18:59:28 +0900

    StackOver scrapping
    StackOver scrapping and make them all in one in csv file

--- indeed.py
+++ indeed.py
@@ -43,7 +43,7 @@ def extract_job(html):
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
-        print(f"Scrapping page {page+1}")
+        print(f"Indeed Scrapping page: {page+1}")
         result = requests.get(f"{URL}&start={page*LIMIT}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div",{"class":"jobsearch-SerpJobCard"})
